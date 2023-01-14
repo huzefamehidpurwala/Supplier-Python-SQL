75c6b332b16ad86c9542a5a30428f24359f13b8d
huzefamehidpurwala <jamaliongithub@gmail.com>
2023-01-14 09:23:02 +0530

    Initial commit

---
+++ commands.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS spj;
+    CREATE DATABASE spj;
+
+USE spj;
+
+create table supplier (
+    SNO varchar(5) Primary key,
+    SNAME varchar(25) NOT NULL,
+    STATUS int NOT NULL,
+    CITY varchar(20) NOT NULL
+);
+
+create table part (
+    PNO varchar(6) Primary key,
+    PNAME varchar(25) NOT NULL,
+    COLOR varchar(10) NOT NULL,
+    WEIGHT numeric(5, 1) NOT NULL,
+    CITY varchar(20) NOT NULL
+);
+
+create table project (
+    JNO varchar(6) Primary key,
+    JNAME varchar(25) NOT NULL,
+    CITY varchar(20) NOT NULL
+);
+
+create table shipment (
+    SNO varchar(6) NOT NULL,
+    PNO varchar(6) NOT NULL,
+    JNO varchar(6) NOT NULL,
+    QTY int NOT NULL,
+    foreign key (SNO) references supplier(SNO),
+    foreign key (PNO) references part(PNO),
+    foreign key (JNO) references project(JNO)
+);
---
+++ main.py
@@ -0,0 +1,46 @@
+import mysql.connector
+
+# Connect to the database
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="7890654321",
+    database="spj"
+)
+
+# Create a cursor object
+cursor = db.cursor()
+
+# Read data from text files
+with open("supplier.txt") as f:
+    supplier_data = f.readlines()
+with open("shipment.txt") as f:
+    shipment_data = f.readlines()
+with open("project.txt") as f:
+    project_data = f.readlines()
+with open("part.txt") as f:
+    part_data = f.readlines()
+
+# Iterate over each line of data and insert it into the corresponding table
+for line in supplier_data:
+    columns = line.strip().split(" ")
+    cursor.execute("INSERT INTO supplier VALUES (%s, %s, %s, %s)", columns)
+
+for line in project_data:
+    columns = line.strip().split(" ")
+    cursor.execute("INSERT INTO project VALUES (%s, %s, %s)", columns)
+
+for line in part_data:
+    columns = line.strip().split(" ")
+    cursor.execute("INSERT INTO part VALUES (%s, %s, %s, %s, %s)", columns)
+
+for line in shipment_data:
+    columns = line.strip().split(" ")
+    cursor.execute("INSERT INTO shipment VALUES (%s, %s, %s, %s)", columns)
+
+# Commit the changes to the database
+db.commit()
+
+# Close the cursor and database connection
+cursor.close()
+db.close()
