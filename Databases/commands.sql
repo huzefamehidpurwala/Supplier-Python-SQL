10066fd902f135800d32cceb704fa1d69f81fbe9
huzefamehidpurwala <jamaliongithub@gmail.com>
2023-01-20 22:48:52 +0530

    Update commands.sql

--- Databases/commands.sql
+++ Databases/commands.sql
@@ -1,44 +1,12 @@
--- DROP DATABASE IF EXISTS spj;
---     CREATE DATABASE spj;
-
-USE mysql;
-SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'spj';
-
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'spj') THEN
-    DROP DATABASE spj;
-END IF;
-
-CREATE DATABASE spj;
+DROP DATABASE IF EXISTS spj;
+    CREATE DATABASE spj;
 
 USE spj;
 
-create table supplier (
-    SNO varchar(5) Primary key,
-    SNAME varchar(25) NOT NULL,
-    STATUS int NOT NULL,
-    CITY varchar(20) NOT NULL
-);
+create table supplier (SNO varchar(5) Primary key, SNAME varchar(25) NOT NULL, STATUS int NOT NULL, CITY varchar(20) NOT NULL);
 
-create table part (
-    PNO varchar(6) Primary key,
-    PNAME varchar(25) NOT NULL,
-    COLOR varchar(10) NOT NULL,
-    WEIGHT numeric(5, 1) NOT NULL,
-    CITY varchar(20) NOT NULL
-);
+create table part (PNO varchar(6) Primary key, PNAME varchar(25) NOT NULL, COLOR varchar(10) NOT NULL, WEIGHT numeric(5, 1) NOT NULL, CITY varchar(20) NOT NULL);
 
-create table project (
-    JNO varchar(6) Primary key,
-    JNAME varchar(25) NOT NULL,
-    CITY varchar(20) NOT NULL
-);
+create table project (JNO varchar(6) Primary key, JNAME varchar(25) NOT NULL, CITY varchar(20) NOT NULL);
 
-create table shipment (
-    SNO varchar(6) NOT NULL,
-    PNO varchar(6) NOT NULL,
-    JNO varchar(6) NOT NULL,
-    QTY int NOT NULL,
-    foreign key (SNO) references supplier(SNO),
-    foreign key (PNO) references part(PNO),
-    foreign key (JNO) references project(JNO)
-);
+create table shipment (SNO varchar(6) NOT NULL, PNO varchar(6) NOT NULL, JNO varchar(6) NOT NULL, QTY int NOT NULL, foreign key (SNO) references supplier(SNO), foreign key (PNO) references part(PNO), foreign key (JNO) references project(JNO));
